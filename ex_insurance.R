3ef6fa1334dee15bd886047f5010b5a7da35fc2c
Qianang Chen <qianang@umich.edu>
2022-12-12 10:26:14 +0000

    Delete ex_insurance.R

--- ex_insurance.R
+++
@@ -1,9 +0,0 @@
-# export data of insurance
-ins = read.csv("desktop/STATS 506/insurance.csv")
-ins = subset(ins,select = -c(1,2,3,4,9))
-names(ins)[2] <- paste("county_fips")
-View(ins)
-county = read.csv("desktop/STATS 506/us_cities.csv")
-ins = merge(county,ins,by=c("county_fips"))
-View(ins)
-write.csv(ins,"desktop/STATS 506/us_insurance.csv")
